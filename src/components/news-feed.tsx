9d8ff72baa964ed48ea05c3ea874c283e5f41200
Ujjwal Krishnamurthi <ujjwal.krishnamurthi@gmail.com>
2020-07-01 15:26:43 -0700

    Adding Alan Button Fix

--- src/components/news-feed.tsx
+++ src/components/news-feed.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import { DataValue } from 'react-apollo';
-
 import { NewsItemModel } from '../data/models';
 import { LoadingSpinner } from './loading-spinner';
 import { NewsDetail, newsDetailNewsItemFragment } from './news-detail';
 import { NewsTitle, newsTitleFragment } from './news-title';
-const alanBtn = require('@alan-ai/alan-sdk-web');
-
+import { useEffect } from 'react';
 export interface INewsFeedProps {
   currentUrl: string;
   first: number;
@@ -18,7 +16,6 @@ export interface INewsFeedProps {
   notice?: JSX.Element;
   skip: number;
 }
-
 export const newsFeedNewsItemFragment = `
   fragment NewsFeed on NewsItem {
     id
@@ -29,7 +26,6 @@ export const newsFeedNewsItemFragment = `
   ${newsTitleFragment}
   ${newsDetailNewsItemFragment}
 `;
-
 export function NewsFeedView(props: INewsFeedProps): JSX.Element {
   const {
     isPostScrutinyVisible = false,
@@ -42,75 +38,77 @@ export function NewsFeedView(props: INewsFeedProps): JSX.Element {
     isUpvoteVisible = true,
     currentUrl,
   } = props;
-
   const nextPage = Math.ceil((skip || 1) / first) + 1;
-  let alanButton = alanBtn({
+  useEffect(() => {
+    console.log('useEffect');
+    const alanBtn = require('@alan-ai/alan-sdk-web');
+    let alanButton = alanBtn({
       key: "868af7a47f1ae52c7628bc60849ece862e956eca572e1d8b807a3e2338fdd0dc/stage",
       rootEl: document.getElementById("alan-btn"),
-  });
+    });
+  },[]);
   console.log(newsItems);
   for (var i = 0; i < newsItems.length; i++) {
-      if (newsItems[i] != null) {
-          console.log(newsItems[i].title);
-      }
+    if (newsItems[i] != null) {
+      console.log(newsItems[i].title);
+    }
   }
   return (
     <tr>
-      <td style={{ padding: '0px' }}>
+        <td style={{ padding: '0px' }}>
         <table
-          style={{
-            border: '0px',
-            padding: '0px',
-            borderCollapse: 'collapse',
-            borderSpacing: '0px',
-          }}
-          className="itemlist"
-        >
-          <tbody>
+              style={{
+                border: '0px',
+                padding: '0px',
+                borderCollapse: 'collapse',
+                borderSpacing: '0px',
+              }}
+              className="itemlist"
+          >
+            <tbody>
             {notice && notice}
             <>
               {newsItems
-                .filter((newsItem): newsItem is NewsItemModel => !!newsItem && !newsItem.hidden)
-                .flatMap((newsItem, index) => [
-                  <NewsTitle
-                    key={`${newsItem.id}title`}
-                    isRankVisible={isRankVisible}
-                    isUpvoteVisible={isUpvoteVisible}
-                    rank={skip + index + 1}
-                    {...newsItem}
-                  />,
-                  <NewsDetail
-                    key={`${newsItem.id}detail`}
-                    isFavoriteVisible={false}
-                    isPostScrutinyVisible={isPostScrutinyVisible}
-                    isJobListing={isJobListing}
-                    {...newsItem}
-                  />,
-                  <tr className="spacer" key={`${newsItem.id}spacer`} style={{ height: 5 }} />,
-                ])}
+                  .filter((newsItem): newsItem is NewsItemModel => !!newsItem && !newsItem.hidden)
+                  .flatMap((newsItem, index) => [
+                    <NewsTitle
+                        key={`${newsItem.id}title`}
+                        isRankVisible={isRankVisible}
+                        isUpvoteVisible={isUpvoteVisible}
+                        rank={skip + index + 1}
+                        {...newsItem}
+                    />,
+                    <NewsDetail
+                        key={`${newsItem.id}detail`}
+                        isFavoriteVisible={false}
+                        isPostScrutinyVisible={isPostScrutinyVisible}
+                        isJobListing={isJobListing}
+                        {...newsItem}
+                    />,
+                    <tr className="spacer" key={`${newsItem.id}spacer`} style={{ height: 5 }} />,
+                  ])}
               <tr key="morespace" className="morespace" style={{ height: '10px' }} />
               <tr key="morelinktr">
                 <td key="morelinkcolspan" colSpan={2} />
                 <td key="morelinktd" className="title">
                   <a
-                    key="morelink"
-                    href={`${currentUrl}?p=${nextPage}`}
-                    className="morelink"
-                    rel="nofollow"
+                      key="morelink"
+                      href={`${currentUrl}?p=${nextPage}`}
+                      className="morelink"
+                      rel="nofollow"
                   >
                     More
                   </a>
                 </td>
               </tr>
             </>
-          </tbody>
-        </table>
-      </td>
-      <div id="alan-btn" />
-    </tr>
+            </tbody>
+          </table>
+        </td>
+        <div id="alan-btn" />
+      </tr>
   );
 }
-
 export interface INewsFeedData {
   error;
   feed;
@@ -126,29 +124,25 @@ export interface INewsFeedContainerProps {
   notice?: JSX.Element;
   skip: number;
 }
-
 export const NewsFeed: React.SFC<INewsFeedContainerProps> = (props) => {
   const { data, currentUrl, first, skip, notice } = props;
-
   if (data?.error) {
     return (
-      <tr>
-        <td>Error loading news items.</td>
-      </tr>
+        <tr>
+          <td>Error loading news items.</td>
+        </tr>
     );
   }
-
   if (data?.feed?.length) {
     return (
-      <NewsFeedView
-        newsItems={data?.feed}
-        currentUrl={currentUrl}
-        first={first}
-        skip={skip}
-        notice={notice}
-      />
+        <NewsFeedView
+            newsItems={data?.feed}
+            currentUrl={currentUrl}
+            first={first}
+            skip={skip}
+            notice={notice}
+        />
     );
   }
-
   return <LoadingSpinner />;
 };
